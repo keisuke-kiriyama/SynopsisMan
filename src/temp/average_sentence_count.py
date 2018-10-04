d353841e7901de97fcda6cf118185712e9fc4166
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2018-10-04 17:21:40 +0900

    :wrench: summarization rate

--- src/temp/average_sentence_count.py
+++ src/temp/average_sentence_count.py
@@ -4,7 +4,10 @@ from util.corpus_accessor import CorpusAccessor
 corpus_accessor = CorpusAccessor()
 
 def synopsis_sentence_count():
-    synopsis_sentence_counts = [len(corpus_accessor.get_synopsis_lines(ncode)) for ncode in corpus_accessor.ncodes]
+    synopsis_sentence_counts = []
+    for ncode in corpus_accessor.ncodes:
+        synopsis_len = len(corpus_accessor.get_synopsis_lines(ncode))
+        synopsis_sentence_counts.append(synopsis_len)
     max_sentence_count = max(synopsis_sentence_counts)
     min_sentence_count = min(synopsis_sentence_counts)
     avg_sentence_cuont = np.average(synopsis_sentence_counts)
@@ -17,6 +20,45 @@ def synopsis_sentence_count():
     for val, key in zip(hist, bins):
         print('sentence count: {} , novel count: {}'.format(key, val))
 
+def contents_sentence_count():
+    contents_sentence_counts = []
+    for ncode in corpus_accessor.ncodes:
+        contents_len = len(corpus_accessor.get_contents_lines(ncode))
+        contents_sentence_counts.append(contents_len)
+    max_sentence_count = max(contents_sentence_counts)
+    min_sentence_count = min(contents_sentence_counts)
+    avg_sentence_cuont = np.average(contents_sentence_counts)
+    bins = np.arange(0, 5000, 100)
+    hist, bins = np.histogram(contents_sentence_counts, bins=bins)
+    print('Max sentence count: ', max_sentence_count)
+    print('Min sentence count: ', min_sentence_count)
+    print('Average sentence count: ', avg_sentence_cuont)
+    print('Histgram')
+    for val, key in zip(hist, bins):
+        print('sentence count: {} , novel count: {}'.format(key, val))
+
+def summarization_rate():
+    rates = []
+    for ncode in corpus_accessor.ncodes:
+        contents_len = len(corpus_accessor.get_contents_lines(ncode))
+        synopsis_len = len(corpus_accessor.get_synopsis_lines(ncode))
+        if contents_len == 0 or synopsis_len == 0: continue
+        rate = synopsis_len / contents_len
+        rates.append(rate)
+    max_rate = max(rates)
+    min_rate = min(rates)
+    avg_rate = np.average(rates)
+    bins = np.arange(0, 0.5, 0.01)
+    hist, bins = np.histogram(rates, bins=bins, density=True)
+    print('Max rate: ', max_rate)
+    print('Min rate: ', min_rate)
+    print('Average sentence count: ', avg_rate)
+    print('Histgram')
+    for val, key in zip(hist, bins):
+        print('rate: {} , novel count: {}'.format(key, val))
+
 
 if __name__ == '__main__':
-    synopsis_sentence_count()
+    # synopsis_sentence_count()
+    # contents_sentence_count()
+    summarization_rate()
