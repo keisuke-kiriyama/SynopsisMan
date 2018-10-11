d6d02d9a0fbe7f836d103854bc25f962b8b0b257
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2018-10-11 19:13:41 +0900

    :wrench: 文数検証

--- src/temp/opt_sentences_data_verification.py
+++ src/temp/opt_sentences_data_verification.py
@@ -23,6 +23,20 @@ def verificate_rouge_score():
     for key, value in zip(b, h):
         print('{:.2f}: {:.1f}%'.format(key, value / total * 100))
 
+@cmd.command()
+def verificate_sentence_count():
+    count = []
+    total = len(corpus_accessor.active_ncodes)
+    for i, ncode in enumerate(corpus_accessor.active_ncodes):
+        if i % 100 == 0:
+            print('[INFO] PROGRESS: {:.1f}'.format(i/total*100))
+        data = opt_sentences_data_supplier.load(ncode)
+        count.append(len(data['opt_sentence_index']))
+    bins = np.arange(1, 7, 1)
+    h, b = np.histogram(count, bins=bins)
+    for key, value in zip(b, h):
+        print('{:.2f}: {}'.format(key, value))
+
 
 def main():
     cmd()
