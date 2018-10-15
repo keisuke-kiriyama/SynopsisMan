ae10ac02d3768508c441ded46d1c0328238bd8e0
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2018-10-15 15:33:05 +0900

    active_ncodesの数確認

--- src/temp/active_ncodes_verification.py
+++ src/temp/active_ncodes_verification.py
@@ -1,5 +1,11 @@
 import joblib
-import os
 
 from util.paths import ACTIVE_NCODES_FILE_PATH
 
+def check_num():
+    with open(ACTIVE_NCODES_FILE_PATH, 'rb') as f:
+        data = joblib.load(f)
+        print(len(data))
+
+if __name__ == '__main__':
+    check_num()
