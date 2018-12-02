90238e370c24db3849e8a30b2ef66d042ec25894
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2018-12-02 13:27:23 +0900

    recursion

--- src/data_construction/rouge_between_contents_and_synopsis_sentence.py
+++ src/data_construction/rouge_between_contents_and_synopsis_sentence.py
@@ -52,7 +52,7 @@ def construct():
     """
     全小説のデータを構築する
     """
-    sys.setrecursionlimit(40000)
+    sys.setrecursionlimit(100000)
     for i, ncode in enumerate(corpus_accessor.ncodes[228300:]):
         print('[INFO] num of constructed data: {}'.format(i))
         rouge_between_contents_and_synopsis_sentence(ncode)
