6aac93d771b07a55594bee551c6d8489128cbe5a
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2018-12-27 17:50:31 +0900

    debug

--- src/temp/opt_sentence_position.py
+++ src/temp/opt_sentence_position.py
@@ -2,6 +2,7 @@ import numpy as np
 import click
 
 from data_supplier import opt_sentences_data_supplier
+from data_supplier.active_ncodes_supplier import get_active_ncodes
 from util.corpus_accessor import CorpusAccessor
 
 corpus_accessor = CorpusAccessor()
@@ -12,21 +13,26 @@ def cmd():
 
 
 @cmd.command()
-def verificate_opt_sentence_position():
-    total_ncodes = len(corpus_accessor.exist_ncodes)
+@click.option('--genre', '-g', default='general')
+def verificate_opt_sentence_position(genre):
+    ncodes = get_active_ncodes(genre)
+    ncodes_count = len(ncodes)
     result = np.zeros(10, dtype=int)
     for i, ncode in enumerate(corpus_accessor.exist_ncodes):
         if i % 100 == 0:
-            print('[INFO] PROGRESS: {:.1f}'.format(i/total_ncodes*100))
+            print('[INFO] PROGRESS: {:.1f}'.format(i/ncodes_count*100))
         contents_lines = corpus_accessor.get_contents_lines(ncode)
+        if len(contents_lines) == 0:
+            return
         positions = opt_sentences_data_supplier.load(ncode)['opt_sentence_index'] / len(contents_lines)
         rounded_positions = [round(value, 1) for value in positions]
         bins = np.arange(0, 1.1, 0.1)
         h, _ = np.histogram(rounded_positions, bins=bins)
         result += h
 
-        if i == 20:
+        if i == 5:
             break
+
     print(result / sum(result))
 
 
